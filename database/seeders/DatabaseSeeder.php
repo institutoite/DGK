16edfec9375af5078cd4c1faf526aec158a6d4c2
institutoite <informaciones.ite@gmail.com>
2025-06-16 12:36:11 -0400

    funcional con su contraseña 2.0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
+            SocialSeeder::class,
             UserSeeder::class,
             InfoSeeder::class,
         ]);
